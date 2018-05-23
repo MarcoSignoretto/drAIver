30b1d9fcfee47f2192c8f7dc2774b69ad65710cc
MarcoSignoretto <850484@stud.unive.it>
2018-05-23 17:25:20 +0200

    Added env file for Giulia's PC

---
+++ draiver/env-ubuntu-giulia.py
@@ -0,0 +1,8 @@
+
+DATASETS_HOME = "/media/marco/Dati/Marco/TesiMarco/Datasets/"
+
+DARKNET_HOME = b"/media/marco/Dati/Marco/TesiMarco/GITProjects/darknet/"
+DARKNET_HOME_NO_BIN = "/media/marco/Dati/Marco/TesiMarco/GITProjects/darknet/"
+
+DARKFLOW_HOME = "/media/marco/Dati/Marco/TesiMarco/GITProjects/darkflow/"
+
